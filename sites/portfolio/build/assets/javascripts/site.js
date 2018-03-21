48a33fcab2a5856f14bf3ab148ddc400d061a355
janzheng <janeazy@gmail.com>
2018-03-21 00:57:02 -0400

    update & sticky

--- sites/portfolio/build/assets/javascripts/site.js
+++ sites/portfolio/build/assets/javascripts/site.js
@@ -10,3 +10,14 @@ $(document).ready(function() {
   // hide loader; reveal page
   window.setTimeout(onLoadPage, 400)
 });
+
+
+$(window).load(function() {
+  $('._size-vheight').each(function() {
+    // calculate vh manually, and set it dynamically
+    // console.log($(this).data())
+    var height = $(window).height() * ($(this).data('vheight') * 0.01);
+    $(this).css({'height': height})
+  });
+});
+
